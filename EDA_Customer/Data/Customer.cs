f0b940769298dea3ab32f8da0c5d0a53f4cca4cf
monzondeveloper <monzondeveloper@gmail.com>
2025-05-29 17:58:54 -0300

    my first commit of the api

---
+++ EDA_Customer/Data/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace EDA_Customer.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Guid ProductId { get; set; }
+        public int ItemInCart { get; set; }
+    }
+}
