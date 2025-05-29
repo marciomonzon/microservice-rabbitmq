51383a2d5e8f31c2765878d26f38d4cb03c96032
monzondeveloper <monzondeveloper@gmail.com>
2025-05-29 18:48:10 -0300

    inventory api

---
+++ EDA_Inventory/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using EDA_Inventory.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace EDA_Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _productDbContext;
+
+        public ProductController(ProductDbContext productDbContext)
+        {
+            _productDbContext = productDbContext;
+        }
+
+        [HttpGet]
+        [Route("/products")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _productDbContext.Products.ToListAsync();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Product>> UpdateProduct(Product product)
+        {
+            _productDbContext.Products.Update(product);
+
+            await _productDbContext.SaveChangesAsync();
+
+            var productSerialized = JsonConvert.SerializeObject(new
+            {
+                product.Id,
+                NewName = product.Name,
+                product.Quantity
+            });
+
+            return CreatedAtAction("GetProducts", new { product.Id }, product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _productDbContext.Products.Add(product);
+            await _productDbContext.SaveChangesAsync();
+
+            var productSerialized = JsonConvert.SerializeObject(new
+            {
+                product.Id,
+                product.ProductId,
+                product.Name,
+                product.Quantity
+            });
+
+            return CreatedAtAction("GetProducts", new { product.Id }, product);
+        }
+    }
+}
---
+++ EDA_Inventory/Data/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EDA_Inventory.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext()
+        {
+        }
+
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
